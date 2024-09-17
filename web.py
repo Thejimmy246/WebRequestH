b5e730632b48dc501aa82e77e8833239a3732d00
Thejimmy246 <l21212021@tectijuana.edu.mx>
2024-09-17 16:01:23 +0000

    Agregamos un nuevo archivo

--- web.py
+++ web.py
@@ -49,6 +49,27 @@ class WebRequestHandler(BaseHTTPRequestHandler):
             self.send_response(200)
             self.send_header("Content-Type", "text/html")
             self.end_headers()
+            
+        elif self.path == "/proyecto/2":
+            self.send_response(200)
+            self.send_header("Content-Type", "text/html")
+            self.end_headers()
+
+            # Leemos el contenido de 1.html
+            try:
+                with open("2.html", "r", encoding="utf-8") as file:
+                    html_content = file.read()
+                # Enviamos el contenido del archivo como respuesta
+                self.wfile.write(html_content.encode("utf-8"))
+            except FileNotFoundError:
+                # Si el archivo no existe, regresamos un error 404
+                self.send_response(404)
+                self.end_headers()
+                self.wfile.write(b"404 - File not found")
+        elif self.path == "/proyecto/web-uno?autor=Jaime":
+            self.send_response(200)
+            self.send_header("Content-Type", "text/html")
+            self.end_headers()
 
             # Leemos el contenido de 1.html
             try:
@@ -68,5 +89,5 @@ class WebRequestHandler(BaseHTTPRequestHandler):
             
 if __name__ == "__main__":
     print("Starting server")
-    server = HTTPServer(("localhost", 8000), WebRequestHandler)
+    server = HTTPServer(("0.0.0.0", 8000), WebRequestHandler)
     server.serve_forever()
